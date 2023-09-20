d9fba3b0770fe279435c5be23ae6014b446c9cc7
Hady Ibrahim <hadyib007@gmail.com>
2023-09-20 17:59:30 -0400

    init, not working yet but not bad

--- SocketCAN/sender/sender.go
+++ SocketCAN/sender/sender.go
@@ -8,11 +8,24 @@ import (
 )
 
 func Send() {
-    // auxMsg := etruckcan.NewAuxiliary().SetHeadLights(etruckcan.Auxiliary_HeadLights_LowBeam)
-    // frame := auxMsg.Frame()
-    conn, _ := socketcan.DialContext(context.Background(), "can", "can0")
+	// auxMsg := etruckcan.NewAuxiliary().SetHeadLights(etruckcan.Auxiliary_HeadLights_LowBeam)
+	// frame := auxMsg.Frame()
+	conn, _ := socketcan.DialContext(context.Background(), "can", "can0")
 
-	frame := can.Frame{}
+	var data *can.Data
+	data.SetBit(0, true)
+	data.SetBit(1, true)
+	data.SetBit(2, true)
+	data.SetBit(3, true)
+	data.SetBit(4, true)
+	data.SetBit(5, true)
+	data.SetBit(6, true)
+	data.SetBit(7, true)
+	frame := can.Frame{
+		ID:     456,
+		Length: 1,
+		Data:   *data,
+	}
 	tx := socketcan.NewTransmitter(conn)
 	_ = tx.TransmitFrame(context.Background(), frame)
 }
--- SocketCAN/tempCodeRunnerFile.go
+++
@@ -1,4 +0,0 @@
- := candevice.New("can0")
-	// _ = d.SetBitrate(250000)
-	// _ = d.SetUp()
-	// defer d.SetDown()

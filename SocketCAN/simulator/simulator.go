6762fa504102318a9482314c02eca971fa7ff2ea
Hady Ibrahim <hadyib007@gmail.com>
2023-09-29 19:21:14 -0400

    test

---
+++ SocketCAN/simulator/simulator.go
@@ -0,0 +1,47 @@
+package tracer
+
+import (
+	"context"
+	"log"
+	"os"
+	"fmt"
+
+	"go.einride.tech/can/pkg/socketcan"
+)
+
+func Trace() {
+	file, err := os.OpenFile("can.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("Failed to open log file: ", err)
+	}
+	defer file.Close()	// need it here so the defer occurs at this end of this not setup()
+
+	log.SetOutput(file) // Set the log output to the file
+
+	conn, err := socketcan.DialContext(context.Background(), "can", "can0")
+	if err != nil {
+			panic(err)
+	}
+
+	rx := socketcan.NewReceiver(conn)
+	for rx.Receive() {
+		frame := rx.Frame()
+
+		log.Println(frame.String())
+		fmt.Println(frame.String())
+	}
+
+	if rx.Err() != nil {
+			panic(err)
+	}
+}
+
+// func setupFile() {
+// 	file, err := os.OpenFile("can.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+// 	if err != nil {
+// 		log.Fatal("Failed to open log file: ", err)
+// 	}
+// 	defer file.Close()
+
+// 	log.SetOutput(file) // Set the log output to the file
+// }
